d5eafd85d29c9adbd48e9df32f786c89af76a45a
Psort <skowronpiotr.00@gmail.com>
2023-10-04 15:04:42 +0200

    implemet method:  King: move  Pawn:move

--- Chess/src/main/java/com/chesstpa/pieces/King.java
+++ Chess/src/main/java/com/chesstpa/pieces/King.java
@@ -6,10 +6,19 @@ import java.util.List;
 
 public class King extends Piece {
 
+    private boolean hasCastling = true;
+
     public King(PieceColor color, Coordinates coordinates) {
         super(color, coordinates);
     }
 
+    @Override
+    public void move(Coordinates coordinates) {
+        if (hasCastling){
+            hasCastling = false;
+        }
+        this.coordinates = coordinates;
+    }
     @Override
     public List<Coordinates> getPossibleMoves(Board board) {
         return null;
--- Chess/src/main/java/com/chesstpa/pieces/Pawn.java
+++ Chess/src/main/java/com/chesstpa/pieces/Pawn.java
@@ -7,10 +7,18 @@ import java.util.List;
 
 public class Pawn extends Piece{
 
+    private boolean hasFirstMove = true;
+
     public Pawn(PieceColor color, Coordinates coordinates) {
         super(color, coordinates);
     }
-
+    @Override
+    public void move(Coordinates coordinates) {
+        if (hasFirstMove){
+            hasFirstMove = false;
+        }
+        this.coordinates = coordinates;
+    }
     @Override
     public List<Coordinates> getPossibleMoves(Board board) {
         return new ArrayList<>(List.of(new Coordinates(4,2)));
