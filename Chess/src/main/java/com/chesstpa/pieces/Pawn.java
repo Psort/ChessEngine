004431d737579dbaa1cb38eb4a38cd5d91e6e783
Psort <skowronpiotr.00@gmail.com>
2023-10-10 01:48:15 +0200

    Refactor method:  PawnMoves:  getPossibleMoves  Pawn:  getPossibleMoves  hasMoved  add:  isFirstMove

--- Chess/src/main/java/com/chesstpa/pieces/Pawn.java
+++ Chess/src/main/java/com/chesstpa/pieces/Pawn.java
@@ -2,12 +2,16 @@ package com.chesstpa.pieces;
 
 import com.chesstpa.board.Board;
 import com.chesstpa.board.Spot;
+import com.chesstpa.moves.KnightMoves;
+import com.chesstpa.moves.PawnMoves;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
-    private boolean hasFirstMove = true;
+    private  final PawnMoves pawnMoves = new PawnMoves();
+    private boolean hasMoved = false;
+    private boolean isFirstMove = false;
 
     public Pawn(PieceColor color) {
         super(color);
@@ -15,12 +19,20 @@ public class Pawn extends Piece {
 
     @Override
     public List<Spot> getPossibleMoves(Board board, Spot spot) {
-        return null;
+        return pawnMoves.getPossibleMoves(board,spot);
     }
-    public boolean doesHasFirstMove(){
-        return hasFirstMove;
+    public boolean hasMoved(){
+        return hasMoved;
     }
-    public void setHasFirstMove(){
-        hasFirstMove = false;
+//    public void setHasMoved(){
+//        hasMoved = false;
+//    }
+
+    public boolean isFirstMove() {
+        return isFirstMove;
+    }
+
+    public void setFirstMove(boolean firstMove) {
+        isFirstMove = firstMove;
     }
 }
