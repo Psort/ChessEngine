9efbf89c7a13d5525d4cd21c7646b9bc06212d2c
Psort <skowronpiotr.00@gmail.com>
2023-10-11 17:25:22 +0200

    add method:     King:     move

--- Chess/src/main/java/com/chesstpa/pieces/King.java
+++ Chess/src/main/java/com/chesstpa/pieces/King.java
@@ -5,6 +5,7 @@ import com.chesstpa.board.Spot;
 import com.chesstpa.moves.KingMoves;
 import com.chesstpa.moves.KnightMoves;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece {
@@ -15,6 +16,16 @@ public class King extends Piece {
 
     public King(PieceColor color) {
         super(color);
+    }
+
+    @Override
+    public void move(Board board,Spot currentSpot,Spot nextSpot) {
+        if(!hasMoved){
+            hasMoved = true;
+        }
+        board.getSpots()[nextSpot.getX()][nextSpot.getY()].setPiece(currentSpot.getPiece());
+        board.getSpots()[currentSpot.getX()][currentSpot.getY()].setPiece(null);
+        board.setKingSpot(nextSpot);
     }
 
     @Override
