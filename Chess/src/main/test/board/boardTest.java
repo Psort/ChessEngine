78740287167358a5b2dfd0f50bdca1bd5153a9c1
Psort <skowronpiotr.00@gmail.com>
2023-10-13 15:58:40 +0200

    Clean Code Add ChessEngine.iml

--- Chess/src/main/test/board/boardTest.java
+++ Chess/src/main/test/board/boardTest.java
@@ -5,9 +5,6 @@ import com.chesstpa.board.Spot;
 import com.chesstpa.pieces.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class boardTest {
