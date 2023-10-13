25ed920a0ac47777caa86e9b2066dc24e7979220
Psort <skowronpiotr.00@gmail.com>
2023-10-13 17:38:21 +0200

    add try to short input in ChessEngine

--- Chess/src/main/java/ChessEngine.java
+++ Chess/src/main/java/ChessEngine.java
@@ -2,13 +2,13 @@ import com.chesstpa.Game;
 
 public class ChessEngine {
     public static void main(String[] args) {
-        if (args.length > 0) {
+        try{
             String boardState = args[0];
             String piecePosition = args[1];
             Game game = new Game();
             String possibleMoves = game.getPossibleMovesForPosition(boardState,piecePosition);
             System.out.println(possibleMoves);
-        } else {
+        }catch (Exception e){
             System.out.println("Usage: java ChessEngine <boardState> <piecePosition>");
         }
     }
