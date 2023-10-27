4a8c2d0239a33bcd9b2953b6a5515064046091c8
Psort <skowronpiotr.00@gmail.com>
2023-10-28 00:29:01 +0200

    add: Game: +setGameState +getBoard +getPossibleMovesForAllPiece +isPat +isCheckMate +board

---
+++ src/main/java/com/chesstpa/game/Game.java
@@ -0,0 +1,45 @@
+package com.chesstpa.game;
+
+import com.chesstpa.board.Board;
+import com.chesstpa.board.Spot;
+import com.chesstpa.pieces.PieceColor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Game {
+    private final Board board = new Board();
+
+    public boolean isCheckMate(PieceColor color){
+        List<Spot> possibleMovesForAllPiece = getPossibleMovesForAllPiece(color);
+        return  board.kingIsCheck(color) && possibleMovesForAllPiece.isEmpty();
+    }
+    public boolean isPat(PieceColor color){
+        Spot[][] spots = board.getSpots();
+        List<Spot> possibleMovesForAllPiece = getPossibleMovesForAllPiece(color);
+        long numberOfPiece = Arrays.stream(spots)
+                .flatMap(Arrays::stream)
+                .filter(spot -> spot.getPiece() != null )
+                .count();
+        return (!board.kingIsCheck(color) && possibleMovesForAllPiece.isEmpty()) || numberOfPiece == 2;
+    }
+
+
+    public List<Spot> getPossibleMovesForAllPiece(PieceColor color){
+        Spot[][] spots = board.getSpots();
+        return Stream.of(spots)
+                .flatMap(Arrays::stream)
+                .filter(spot -> spot.getPiece() != null && spot.getPiece().getColor() == color)
+                .flatMap(spot -> spot.getPiece().getPossibleMoves(board, spot).stream())
+                .toList();
+    }
+
+    public void setGameState(String boardState, String whiteCastle, String blackCastle) {
+        board.setBoardState(boardState,whiteCastle,blackCastle);
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+}
