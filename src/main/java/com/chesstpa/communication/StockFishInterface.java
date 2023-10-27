06b4ab81b68144e1a8ea4dc38b1779844be86e76
Psort <skowronpiotr.00@gmail.com>
2023-10-28 00:20:52 +0200

    delete: StockFishInterface.java

--- src/main/java/com/chesstpa/communication/StockFishInterface.java
+++
@@ -1,8 +0,0 @@
-package com.chesstpa.communication;
-
-public class StockFishInterface implements CommunicationInterface{
-    @Override
-    public String getPossibleMovesForPosition(String boardState, String piecePosition) {
-        return null;
-    }
-}
