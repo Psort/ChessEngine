8f77e9f253d8e81302e3735c241f513ae8f94573
Psort <skowronpiotr.00@gmail.com>
2023-11-01 11:16:56 +0100

    refactor: Spot-> isBeaten

--- src/main/java/com/chesstpa/board/Spot.java
+++ src/main/java/com/chesstpa/board/Spot.java
@@ -2,11 +2,6 @@ package com.chesstpa.board;
 
 import com.chesstpa.pieces.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Spot {
     private Piece piece;
     private final Position position;
@@ -25,18 +20,57 @@ public class Spot {
         return position;
     }
 
-    public boolean isBeaten(Board board, PieceColor color) {
-        int x = position.getX();
-        int y = position.getY();
-        Piece heldPiece = this.piece;
-        setPiece(null);
-        List<Spot> allPossibleMoves =  Stream.of(board.getSpots())
-                .flatMap(Arrays::stream)
-                .filter(spot -> spot.getPiece() != null && spot.getPiece().getColor() != color)
-                .flatMap(spot -> spot.getPiece().getBeatenSpot(board, spot).stream())
-                .toList();
-        setPiece(heldPiece);
-        return allPossibleMoves.stream().anyMatch(spot -> spot.getPosition().getX()==x &&  spot.getPosition().getY()==y);
+    public boolean isBeaten(Board board,PieceColor color) {
+        PieceType pawnClass = (color == PieceColor.White)?PieceType.WHITEPAWN:PieceType.BLACKPAWN;
+
+        return isBeaten(color,board,PieceType.KNIGHT) ||isBeaten(color,board,PieceType.KING)
+                || isBeaten(color,board,pawnClass) || isBeaten(color, board);
+    }
+
+    private boolean isBeaten(PieceColor color, Board board) {
+        return isBeatenByDirection(color, board, -1, 0) || isBeatenByDirection(color, board, 0, -1)
+                || isBeatenByDirection(color, board, 0, 1) || isBeatenByDirection(color, board, 1, 0)
+                || isBeatenByDirection(color, board, -1, -1) || isBeatenByDirection(color, board, -1, 1)
+                || isBeatenByDirection(color, board, 1, -1) || isBeatenByDirection(color, board, 1, 1);
+    }
+    private boolean isBeatenByDirection(PieceColor color, Board board, int rowChange, int colChange) {
+        int newX = position.getX() + rowChange;
+        int newY = position.getY() + colChange;
+
+        while (isValidCoordinate(newX, newY)) {
+            Spot currentSpot = board.getSpot(newX, newY);
+            if (!currentSpot.isEmpty()) {
+                if (currentSpot.getPiece().getColor() != color &&
+                        ((rowChange == 0 || colChange == 0) && (currentSpot.getPiece() instanceof Rook || currentSpot.getPiece() instanceof Queen) ||
+                                (Math.abs(rowChange) == Math.abs(colChange) && (currentSpot.getPiece() instanceof Bishop || currentSpot.getPiece() instanceof Queen)))) {
+                    return true;
+                }
+                break;
+            }
+            newX += rowChange;
+            newY += colChange;
+        }
+
+        return false;
+    }
+    private boolean isBeaten(PieceColor color, Board board,PieceType pieceType) {
+        int[][] moves = pieceType.getMoves();
+
+        for (int[] move : moves) {
+            int newX = position.getX() + move[0];
+            int newY = position.getY() + move[1];
+            if (isValidCoordinate(newX, newY) && isOpponent(board.getSpot(newX, newY), color,pieceType.getaClass())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isOpponent(Spot spot, PieceColor color,Class opponent) {
+        return spot.getPiece() !=  null && spot.getPiece().getClass() == opponent && spot.getPiece().getColor() != color;
+    }
+
+    private boolean isValidCoordinate(int x, int y) {
+        return x >= 0 && x < Board.SIZE && y >= 0 && y < Board.SIZE;
     }
 
     public boolean isEmpty() {
@@ -45,9 +79,10 @@ public class Spot {
 
     public boolean safeKing(Board board, PieceColor color,Piece piece) {
         Spot kingSpot = board.getKingSpot(color);
+        Piece oldPiece = this.piece;
         this.piece = piece;
         boolean itSafe = !kingSpot.isBeaten(board,color);
-        this.piece = null;
+        this.piece = oldPiece;
         return !itSafe;
     }
     public boolean revealsKing(Board board, PieceColor color, Piece piece, Spot previousSpot){
