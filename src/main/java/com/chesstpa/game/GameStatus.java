fdfd6021f9b1342fa451f267c65bc8d1412754a6
Psort <skowronpiotr.00@gmail.com>
2023-11-12 19:13:41 +0100

    refactor GameStatus: add Value

--- src/main/java/com/chesstpa/game/GameStatus.java
+++ src/main/java/com/chesstpa/game/GameStatus.java
@@ -1,7 +1,17 @@
 package com.chesstpa.game;
 
 public enum GameStatus {
-    CHECKMATE,
-    PAT,
-    GAME
+    CHECKMATE("Checkmate"),
+    PAT("Pat"),
+    GAME("Game");
+
+    private final String value;
+
+    GameStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
