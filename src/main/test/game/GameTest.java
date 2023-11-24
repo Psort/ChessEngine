97fc9dd4ccb3404cb700b3ac076f0b6cd11aeaea
Psort <skowronpiotr.00@gmail.com>
2023-11-24 19:49:49 +0100

    refactor: GameTest: whiteCastle , BlackCastle -> castle

--- src/main/test/game/GameTest.java
+++ src/main/test/game/GameTest.java
@@ -21,34 +21,34 @@ public class GameTest {
     @Test
     void isCheckMateTest() {
         //initialize Piece
-        game.setGameState("k5rr/8/8/8/8/8/8/7K","","");
+        game.setGameState("k5rr/8/8/8/8/8/8/7K","");
         assertTrue(game.isCheckMate(PieceColor.White));
     }
     @Test
     void isNotCheckMateTest() {
         //initialize Piece
-        game.setGameState("K5r1/8/8/8/8/8/8/7k","","");
+        game.setGameState("K5r1/8/8/8/8/8/8/7k","");
         assertFalse(game.isCheckMate(PieceColor.White));
 
     }
     @Test
     void isPatTest() {
-        game.setGameState("K7/6r1/8/8/8/8/1r7/7k","","");
+        game.setGameState("K7/6r1/8/8/8/8/1r7/7k","");
         assertTrue(game.isPat(PieceColor.White));
     }
     @Test
     void isPatWithTwoKingsTest() {
-        game.setGameState("K7/8/8/8/8/8/8/7k","","");
+        game.setGameState("K7/8/8/8/8/8/8/7k","");
         assertTrue(game.isPat(PieceColor.White));
     }
     @Test
     void isNotPatTest() {
-        game.setGameState("K5r1/8/8/8/8/8/8/r6k","","");
+        game.setGameState("K5r1/8/8/8/8/8/8/r6k","");
         assertFalse(game.isPat(PieceColor.White));
     }
     @Test
     void isScholarsMateTest() {
-        game.setGameState("3qkb2/3ppQ/4Q3/8/8/8/8/7K","","");
+        game.setGameState("3qkb2/3ppQ/4Q3/8/8/8/8/7K","");
         List<Spot> takenPositions = game.getBoard().getSpot(0,4).getPiece().getPossibleMoves(game.getBoard(),game.getBoard().getSpot(0,4));
         assertTrue(game.isCheckMate(PieceColor.Black));
     }
