b51cff2031376064a32eb05ec9e122e2159209db
Psort <skowronpiotr.00@gmail.com>
2024-03-07 22:13:46 +0100

    CommunicationInterface ,ChessEngine: +getAllPossibleMovesForColor

--- src/main/java/com/chesstpa/communication/ChessEngine.java
+++ src/main/java/com/chesstpa/communication/ChessEngine.java
@@ -1,12 +1,16 @@
 package com.chesstpa.communication;
 
 import com.chesstpa.DataConvert;
+import com.chesstpa.board.Board;
 import com.chesstpa.game.Game;
 import com.chesstpa.board.Position;
 import com.chesstpa.board.Spot;
 import com.chesstpa.game.GameStatus;
 import com.chesstpa.pieces.PieceColor;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -34,9 +38,25 @@ public class ChessEngine implements CommunicationInterface{
     }
 
     @Override
+    public Map<Position, List<Position>> getAllPossibleMovesForColor(String boardState, String piecesColor, String castle) {
+        PieceColor color = Objects.equals(piecesColor, "WHITE") ?PieceColor.WHITE:PieceColor.BLACK;
+        game.setGameState(boardState,castle);
+        Board board = game.getBoard();
+        return Arrays.stream(game.getBoard().getSpots())
+                .flatMap(Arrays::stream)
+                .filter(spot -> spot.getPiece() != null && spot.getPiece().getColor() == color)
+                .collect(Collectors.groupingBy(
+                        Spot::getPosition,
+                        Collectors.flatMapping(spot -> spot.getPiece().getPossibleMoves(board, spot).stream()
+                                        .map(Spot::getPosition),
+                                Collectors.toList())
+                ));
+    }
+
+    @Override
     public String getGameStatus(String boardState,String castle,String color) {
         game.setGameState(boardState,castle);
-        PieceColor pieceColor = Objects.equals(color, "WHITE") ?PieceColor.White:PieceColor.Black;
+        PieceColor pieceColor = Objects.equals(color, "WHITE") ?PieceColor.WHITE:PieceColor.BLACK;
         if (game.isCheckMate(pieceColor)){
             return GameStatus.CHECKMATE.getValue();
         }
--- src/main/java/com/chesstpa/communication/CommunicationInterface.java
+++ src/main/java/com/chesstpa/communication/CommunicationInterface.java
@@ -1,9 +1,14 @@
 package com.chesstpa.communication;
 
+import com.chesstpa.board.Position;
 import com.chesstpa.game.GameStatus;
+
+import java.util.List;
+import java.util.Map;
 
 public interface CommunicationInterface {
 
-    public String getPossibleMovesForPosition(String boardState, String piecePosition,String castle);
-    public String getGameStatus(String boardState, String castle,String color);
+     String getPossibleMovesForPosition(String boardState, String piecePosition,String castle);
+     Map<Position, List<Position>> getAllPossibleMovesForColor(String boardState, String piecesColor, String castle);
+     String getGameStatus(String boardState, String castle,String color);
 }
